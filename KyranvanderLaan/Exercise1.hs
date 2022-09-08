cb7241bb894757d397c71962d00d017e1bf33d85
Kyran <kyransem@gmail.com>
2022-09-08 12:26:57 +0200

    Finished ex1

--- KyranvanderLaan/Exercise1.hs
+++ KyranvanderLaan/Exercise1.hs
@@ -1,6 +1,6 @@
 -- Base code imported from Lab 1 at https://canvas.uva.nl/courses/32483/files/7147680?wrap=1&fd_cookie_set=1
 
--- module Exercise1 where
+module Exercise1 where
 import Data.List
 import Test.QuickCheck    
 
@@ -26,17 +26,16 @@ data Boy = Matthew | Peter | Jack | Arnold | Carl
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
 -- My thoughts:
--- The first property I want to test is whether the statement that 
--- 1^2+2^2+...+n^2 = (n*(n+1)*((2*n)+1))/6 holds with quickcheck.
--- I will do this by implementing both sides of the formula and checking if they are equal.
+-- I want to test is whether the statements that 
+-- 1^2+2^2+...+n^2 = (n*(n+1)*((2*n)+1))/6 and 1^3+2^3+···+n^3 = ((n*(n+1))/2)^2 hold.
+-- I will do this by implementing both sides of the formulas and checking if they are equal with quickcheck.
 -- Initially I output a list of integers to confirm the lists with powers are correct.
 -- I needed to take node that only natural numbers should be used so 0 and minus ints aren't base cases.  
 -- An initial quickCheck failed because the constraint for only natural numbers hasn't been made yet.
 -- The inspiration for the starting of the code and the usage of abs for natural numbers came from the 
 -- QuickCheck pdf from canvas: https://canvas.uva.nl/courses/32483/files/7077441?module_item_id=1412882.
--- Test returned 100 passed tests for both exercises 2 and 3
--- The second property I want to test is 
-
+-- Personally it took some time for me to process how the positive integer generator worked.
+-- Tests returned 100 passed tests for both exercises 2 and 3
 -- Time it took: 40 minutes.
 
 sumNums2 :: Integer -> Integer
@@ -45,20 +44,23 @@ sumNums2 n = sum [k^2 | k <- [1..n]]
 sumNums2' :: Integer -> Integer
 sumNums2' n = div (n * (n+1) * ((2*n)+1)) 6
 
-testsumNums2 :: Integer -> Bool
-testsumNums2 n = let x = abs n in sumNums2 x == sumNums2' x
-
 sumNums3 :: Integer -> Integer
 sumNums3 n = sum [k^3 | k <- [1..n]]
 
 sumNums3' :: Integer -> Integer
 sumNums3' n = (div (n*(n+1)) 2)^2
 
+genPositiveIntegers :: Gen Integer
+genPositiveIntegers = abs <$> (arbitrary :: Gen Integer) `suchThat` (>0)
+
+testsumNums2 :: Integer -> Bool
+testsumNums2 n = sumNums2 n == sumNums2' n
+
 testsumNums3 :: Integer -> Bool
 testsumNums3 n = let x = abs n in sumNums3 x ==  sumNums3' x
 
 main :: IO ()
 main = do
-  putStrLn "== Proof induction =="
-  quickCheck testsumNums2
-  quickCheck testsumNums3
+  putStrLn "== Check test =="
+  quickCheck $ forAll genPositiveIntegers testsumNums2
+  quickCheck $ forAll genPositiveIntegers testsumNums3
