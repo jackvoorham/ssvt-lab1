7e32ccab27c0313b6b1b327bd02a843cda1094ff
Chris Gunawardena <chris.gunawardena@ing.com>
2022-09-09 22:07:30 +0200

    q1

---
+++ ChrisGunawardena/Exercise2.hs
@@ -0,0 +1,4 @@
+
+-- Redo exercise 4 of Workshop 1 and test the property for integer lists of the form [1..n]. You can use subsequences :: [a] -> [[a]] for the list of all subsequences of a given list.
+
+
